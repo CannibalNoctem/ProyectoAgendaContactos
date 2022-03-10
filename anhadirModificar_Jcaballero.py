5a045d58816f1a11d0fff4bcaf822373a489dbe8
Jorge M <jorge.docencia@gmail.com>
2022-03-10 09:08:10 +0100

    Update anhadirModificar_Jcaballero.py

--- anhadirModificar_Jcaballero.py
+++ anhadirModificar_Jcaballero.py
@@ -1,5 +1,5 @@
 #Este es el código de Chema de la función de añadir modificar# 
-def añadirModificarJcaballero(nombre, agenda):
+def anhadirModificarJcaballero(nombre, agenda):
     if nombre in agenda:
         print("%s es un contacto que ya existe, su número de teléfono es %s" % (nombre,agenda[nombre]))
         selec = input("Pulsa la tecla 's' si quiere modificarlo. Cualquier otra tecla para continuar.")
