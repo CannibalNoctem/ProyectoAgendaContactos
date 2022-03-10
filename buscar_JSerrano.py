c291381f794518a8fd50afa1d3df41ac4437e16c
Jorge M <jorge.docencia@gmail.com>
2022-03-10 13:30:05 +0100

    Add files via upload

---
+++ buscar_JSerrano.py
@@ -0,0 +1,6 @@
+#Esta programa fue creado el martes 8 de marzo de 2022
+#Este es el programa de Jaime Serrano Maya y definimos y ponemos en acción la opción buscar dentro de la agenda
+def buscar_JSerrano(nombre, agenda):    #Definimos la operación de buscar y sus variables, nombre y agenda
+    for nombre, numtel in agenda.items(): 
+        if nombre.startswith(cadena):
+            print("El número de teléfono de %s es el %s" % (nombre, agenda [nombre]))   #Mostramos la frase con el % que es definido en la misma línea como nombre y número
