e50f25f35794dc3c53f3ce1322b02072220134a2
Jorge M <jorge.docencia@gmail.com>
2022-03-10 09:04:01 +0100

    Add files via upload

---
+++ anhadirModificar_Jcaballero.py
@@ -0,0 +1,11 @@
+#Este es el código de Chema de la función de añadir modificar# 
+def añadirModificarJcaballero(nombre, agenda):
+    if nombre in agenda:
+        print("%s es un contacto que ya existe, su número de teléfono es %s" % (nombre,agenda[nombre]))
+        selec = input("Pulsa la tecla 's' si quiere modificarlo. Cualquier otra tecla para continuar.")
+        if selec == "s":
+            telefono = input("Dime el nuevo número de teléfono: ")
+            agenda[nombre]=telefono
+    else:
+        telefono = input("Dime el número de teléfono: ")
+        agenda[nombre]=telefono
---
+++ buscar_ASerrano.py
@@ -0,0 +1,9 @@
+#Este es el código de la búsqueda de Andrés Serrano Rodríguez, sirve para realizar búsquedas en la agenda a partir de nombres de usuarios.
+def buscar_ASerrano(nombre, agenda):
+        for nombre in agenda.items():
+            if nombre.startswith(búsqueda):
+                print("Estos son los contactos " % (nombre,agenda[nombre]))   
+
+
+
+
